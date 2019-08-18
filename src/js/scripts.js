2d4294fbb8c07231eeb4b39ed6366ff99553045c
Artemy <owleek@mail.ru>
2019-08-18 18:13:09 +0300

    почти готовая карусель

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+        }
+    })
 });
